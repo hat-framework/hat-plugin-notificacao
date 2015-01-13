ea02804c62660fe861d70032360a13c3c6ec12c7
tigredonorte <tigredonorte3@gmail.com>
2015-01-13 16:39:48 -0200

    permname

--- Config/notificacaoActions.php
+++ Config/notificacaoActions.php
@@ -4,14 +4,14 @@ class notificacaoActions extends Actions{
     protected $permissions = array(
         "notify_user" => array(
             "nome"      => "notify_user",
-            "label"     => "Notificação para usuários",
-            "descricao" => "Permite enviar notificações em tempo real para os usuários.",
+            "label"     => "NotificaÃ§Ã£o para usuÃ¡rios",
+            "descricao" => "Permite enviar notificaÃ§Ãµes em tempo real para os usuÃ¡rios.",
             'default'   => 's',
         ),
     );
     protected $actions = array(
         'notificacao/notifycount/load' => array(
-            'label' => 'Quantidade de Notificações', 'publico' => 's', "default_yes" => "s","default_no" => "n",
+            'label' => 'Quantidade de NotificaÃ§Ãµes', 'publico' => 's', "default_yes" => "s","default_no" => "n",
             "permission" => "notify_user", 'needcod' => false,
         ),
     );
